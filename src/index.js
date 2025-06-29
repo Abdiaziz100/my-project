cf06379ece6ad6316eedbc2554828d38dc34a4c5
mohamed <abdiazizmahat@gmail.com>
2025-06-29 14:30:43 +0300

    Create searchMeal function with API fetch and basi c validation

--- src/index.js
+++ src/index.js
@@ -7,3 +7,24 @@ input.focus();
 
 // Base API URL for meal search
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+//main function to fetch and display meal search results
+function searchMeal() {
+  input.focus(); // Focus input field again after search
+  let userValue = document.querySelector("#user-input").value;
+
+  // If input is empty, show warning
+  if (userValue.length == 0) {
+    result.innerHTML = `<h3>Input Field Cannot be Empty</h3>`;
+  } else {
+    // Fetch data from the API
+    fetch(url + userValue)
+      .then((response) => response.json())
+      .then((data) => {
+        // If meal not found, throw error
+        if (!data.meals) throw "No meal found";
+
+        // Get the first meal result
+        let myMeal = data.meals[0];
+        console.log(myMeal); // Log meal info in console
+
