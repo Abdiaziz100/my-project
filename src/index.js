c00a65c073dd6690f67cafff11a97377cfa34871
mohamed <abdiazizmahat@gmail.com>
2025-06-29 14:32:47 +0300

    Parse meal ingredients and display meal data in th e DOM

--- src/index.js
+++ src/index.js
@@ -28,3 +28,35 @@ function searchMeal() {
         let myMeal = data.meals[0];
         console.log(myMeal); // Log meal info in console
 
+        // Prepare ingredients list
+        let count = 1;
+        let ingredients = [];
+        for (let i in myMeal) {
+          let ingredient = "";
+          let measure = "";
+
+          // Look for all "strIngredient" fields that have a value
+          if (i.startsWith("strIngredient") && myMeal[i]) {
+            ingredient = myMeal[i];
+            measure = myMeal["strMeasure" + count];
+            count += 1;
+
+            // Combine measure with ingredient (e.g. "1 cup Flour")
+            ingredients.push(`${measure} ${ingredient}`);
+          }
+        }
+
+        // Update result section with meal info
+        result.innerHTML = `
+          <img src=${myMeal.strMealThumb}>
+          <div class="details">
+            <h2>${myMeal.strMeal}</h2>
+            <h4>${myMeal.strArea}</h4>
+          </div>
+          <div class="ingredient-content"></div>
+          <div class="recipe">
+            <button id="hide-recipe">X</button>
+            <pre id="instructions">${myMeal.strInstructions}</pre>
+          </div>
+          <button id="show-recipe">View Recipe</button>
+        `;
